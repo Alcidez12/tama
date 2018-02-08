901e084e3fe47867015de061d2ce6fa7ef16c8aa
daswolke <wolke@weeb.sh>
2018-02-08 16:52:40 +0100

    apply js-standard and add a limit of 30KB per document

--- src/DB/setting.mongo.js
+++ src/DB/setting.mongo.js
@@ -1,9 +1,9 @@
-let mongoose = require('mongoose');
-let settingsSchema = mongoose.Schema({
-    accountId: String,
-    type: String,
-    id: String,
-    data: {}
-});
-let settingsModel = mongoose.model('settings', settingsSchema);
-module.exports = settingsModel;
+const mongoose = require('mongoose')
+const settingsSchema = mongoose.Schema({
+  accountId: String,
+  type: String,
+  id: String,
+  data: {}
+})
+const settingsModel = mongoose.model('settings', settingsSchema)
+module.exports = settingsModel
--- src/controller/setting.controller.js
+++ src/controller/setting.controller.js
@@ -1,93 +1,92 @@
-const SettingModel = require('../DB/setting.mongo');
-const SubsettingModel = require('../DB/subsetting.mongo');
+const SettingModel = require('../DB/setting.mongo')
+const SubsettingModel = require('../DB/subsetting.mongo')
 
 class SettingsController {
-    async getSetting(settingType, settingId, accountId) {
-        return SettingModel.findOne({type: settingType, id: settingId, accountId}, {_id: 0, __v: -0});
-    }
+  async getSetting (settingType, settingId, accountId) {
+    return SettingModel.findOne({type: settingType, id: settingId, accountId}, {_id: 0, __v: -0})
+  }
 
-    async updateSetting(settingType, settingId, accountId, settingData) {
-        let setting = await this.getSetting(settingType, settingId, accountId);
-        if (setting) {
-            await this._updateSetting(settingType, settingId, accountId, settingData);
-            setting.data = settingData;
-            return setting;
-        }
-        return this._createSetting(settingType, settingId, accountId, settingData)
+  async updateSetting (settingType, settingId, accountId, settingData) {
+    const setting = await this.getSetting(settingType, settingId, accountId)
+    if (setting) {
+      await this._updateSetting(settingType, settingId, accountId, settingData)
+      setting.data = settingData
+      return setting
     }
+    return this._createSetting(settingType, settingId, accountId, settingData)
+  }
 
-    async _createSetting(settingType, settingId, accountId, settingData) {
-        let setting = new SettingModel({
-            id: settingId,
-            type: settingType,
-            accountId,
-            data: settingData
-        });
-        await setting.save();
-        return setting;
+  async _createSetting (settingType, settingId, accountId, settingData) {
+    const setting = new SettingModel({
+      id: settingId,
+      type: settingType,
+      accountId,
+      data: settingData
+    })
+    await setting.save()
+    return setting
+  }
+
+  async _updateSetting (settingType, settingId, accountId, settingData) {
+    return SettingModel.update({id: settingId, type: settingType, accountId}, {$set: {data: settingData}})
+  }
+
+  async deleteSetting (settingType, settingId, accountId) {
+    const setting = await this.getSetting(settingType, settingId, accountId)
+    if (setting) {
+      await this._deleteSetting(settingType, settingId, accountId)
+      return setting
     }
+    return null
+  }
 
-    async _updateSetting(settingType, settingId, accountId, settingData) {
-        return SettingModel.update({id: settingId, type: settingType, accountId}, {$set: {data: settingData}});
+  async _deleteSetting (settingType, settingId, accountId) {
+    return SettingModel.remove({id: settingId, type: settingType, accountId})
+  }
+
+  async getSubsettings (settingType, settingId, accountId, subType) {
+    return SubsettingModel.find({type: settingType, id: settingId, accountId, subType}, {_id: 0, __v: 0})
+  }
+
+  async getSubsetting (settingType, settingId, accountId, subType, subId) {
+    return SubsettingModel.findOne({type: settingType, id: settingId, accountId, subType, subId}, {_id: 0, __v: 0})
+  }
+
+  async updateSubsetting (settingType, settingId, accountId, subType, subId, data) {
+    const subsetting = await this.getSubsetting(settingType, settingId, accountId, subType, subId)
+    if (subsetting) {
+      await this._updateSubsetting(settingType, settingId, accountId, subType, subId, data)
+      subsetting.data = data
+      return subsetting
     }
+    return this._createSubsetting(settingType, settingId, accountId, subType, subId, data)
+  }
 
-    async deleteSetting(settingType, settingId, accountId) {
-        let setting = await this.getSetting(settingType, settingId, accountId);
-        if (setting) {
-            await this._deleteSetting(settingType, settingId, accountId);
-            return setting;
-        }
-        return null;
+  async _updateSubsetting (settingType, settingId, accountId, subType, subId, data) {
+    return SubsettingModel.update({type: settingType, id: settingId, accountId, subType, subId}, {$set: {data}})
+  }
+
+  async _createSubsetting (settingType, settingId, accountId, subType, subId, data) {
+    const setting = new SubsettingModel({
+      id: settingId,
+      type: settingType,
+      accountId,
+      data,
+      subId,
+      subType
+    })
+    await setting.save()
+    return setting
+  }
+
+  async deleteSubsetting (settingType, settingId, accountId, subType, subId) {
+    const subsetting = await this.getSubsetting(settingType, settingId, accountId, subType, subId)
+    if (subsetting) {
+      await SubsettingModel.remove({type: settingType, id: settingId, accountId, subType, subId})
+      return subsetting
     }
-
-    async _deleteSetting(settingType, settingId, accountId) {
-        return SettingModel.remove({id: settingId, type: settingType, accountId});
-    }
-
-    async getSubsettings(settingType, settingId, accountId, subType) {
-        return SubsettingModel.find({type: settingType, id: settingId, accountId, subType}, {_id: 0, __v: 0});
-    }
-
-    async getSubsetting(settingType, settingId, accountId, subType, subId) {
-        return SubsettingModel.findOne({type: settingType, id: settingId, accountId, subType, subId}, {_id: 0, __v: 0});
-    }
-
-    async updateSubsetting(settingType, settingId, accountId, subType, subId, data) {
-        let subsetting = await this.getSubsetting(settingType, settingId, accountId, subType, subId);
-        if (subsetting) {
-            await this._updateSubsetting(settingType, settingId, accountId, subType, subId, data);
-            subsetting.data = data;
-            return subsetting;
-        }
-        return this._createSubsetting(settingType, settingId, accountId, subType, subId, data);
-    }
-
-    async _updateSubsetting(settingType, settingId, accountId, subType, subId, data) {
-        return SubsettingModel.update({type: settingType, id: settingId, accountId, subType, subId}, {$set: {data}})
-    }
-
-    async _createSubsetting(settingType, settingId, accountId, subType, subId, data) {
-        let setting = new SubsettingModel({
-            id: settingId,
-            type: settingType,
-            accountId,
-            data,
-            subId,
-            subType
-        });
-        await setting.save();
-        return setting;
-    }
-
-    async deleteSubsetting(settingType, settingId, accountId, subType, subId) {
-        let subsetting = await this.getSubsetting(settingType, settingId, accountId, subType, subId);
-        if (subsetting) {
-            await SubsettingModel.remove({type: settingType, id: settingId, accountId, subType, subId});
-            return subsetting;
-        }
-        return null;
-    }
-
+    return null
+  }
 }
 
-module.exports = new SettingsController();
+module.exports = new SettingsController()
